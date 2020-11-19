061d8c17e02691df4edddbb23a82d1be500b3e07
ngeframetd <70860536+ngeframetd@users.noreply.github.com>
2020-11-19 17:30:22 +0700

    Update authorize.py

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -1,6 +1,6 @@
 from bot.helper.telegram_helper.message_utils import sendMessage
 from telegram.ext import run_async
-from bot import AUTHORIZED_CHATS, SUDO_USERS, dispatcher
+from bot import AUTHORIZED_CHATS, dispatcher
 from telegram.ext import CommandHandler
 from bot.helper.telegram_helper.filters import CustomFilters
 from telegram import Update
@@ -68,77 +68,13 @@ def unauthorize(update,context):
     sendMessage(msg, context.bot, update)
 
 
-@run_async
-def addSudo(update,context):
-    reply_message = None
-    message_ = None
-    reply_message = update.message.reply_to_message
-    message_ = update.message.text.split(' ')
-    if len(message_) == 2:
-        chat_id = int(message_[1])
-        if chat_id not in SUDO_USERS:
-            msg = DbManger().db_addsudo(chat_id)
-        else:
-            msg = 'Already Sudo'
-    else:
-        if reply_message is None:
-            msg = "Give ID or Reply To message of whom you want to Promote"
-        else:
-            # Trying to authorize someone in specific
-            user_id = reply_message.from_user.id
-            if user_id not in SUDO_USERS:
-                msg = DbManger().db_addsudo(user_id)
-            else:
-                msg = 'Already Sudo'
-    sendMessage(msg, context.bot, update)
-
-
-@run_async
-def removeSudo(update,context):
-    reply_message = None
-    message_ = None
-    reply_message = update.message.reply_to_message
-    message_ = update.message.text.split(' ') 
-    if len(message_) == 2:
-        chat_id = int(message_[1])
-        if chat_id in SUDO_USERS:
-            msg = DbManger().db_rmsudo(chat_id)
-        else:
-            msg = 'Not a Sudo'
-    else:
-        if reply_message is None:
-            msg = "Give ID or Reply To message of whom you want to remove from Sudo"
-        else:
-            user_id = reply_message.from_user.id
-            if user_id in SUDO_USERS:
-                msg = DbManger().db_rmsudo(user_id)
-            else:
-                msg = 'Not a Sudo'
-    sendMessage(msg, context.bot, update)
-
-
-@run_async
-def sendAuthChats(update,context):
-    user = sudo = ''
-    user += '\n'.join(str(id) for id in AUTHORIZED_CHATS)
-    sudo += '\n'.join(str(id) for id in SUDO_USERS)
-    sendMessage(f'<b><u>Authorized Chats</u></b>\n{user}\n<b><u>Sudo Users</u></b>\n{sudo}', context.bot, update)
-
-
 send_auth_handler = CommandHandler(command=BotCommands.AuthorizedUsersCommand, callback=sendAuthChats,
-                                    filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
+                                    filters=CustomFilters.owner_filter)
 authorize_handler = CommandHandler(command=BotCommands.AuthorizeCommand, callback=authorize,
-                                   filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
+                                   filters=CustomFilters.owner_filter)
 unauthorize_handler = CommandHandler(command=BotCommands.UnAuthorizeCommand, callback=unauthorize,
-                                     filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
-addsudo_handler = CommandHandler(command=BotCommands.AddSudoCommand, callback=addSudo,
-                                    filters=CustomFilters.owner_filter)
-removesudo_handler = CommandHandler(command=BotCommands.RmSudoCommand, callback=removeSudo,
-                                    filters=CustomFilters.owner_filter)
+                                     filters=CustomFilters.owner_filter)
 
 dispatcher.add_handler(send_auth_handler)
 dispatcher.add_handler(authorize_handler)
 dispatcher.add_handler(unauthorize_handler)
-dispatcher.add_handler(addsudo_handler)
-dispatcher.add_handler(removesudo_handler)
-
